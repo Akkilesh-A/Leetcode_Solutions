bf0b7b53206abfed5d77c758078b7b7b42c92372
Spacecentre <136828513+Akkilesh-A@users.noreply.github.com>
2024-01-30 19:18:22 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (15.19%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,23 +1,10 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<=INT_MAX && n>=INT_MIN){
-            if(n==1){
-                return true;
-            }
-            else if(n%2!=0){
-                return false;
-            }
-            else{
-                while(n%2==0 && n!=0){
-                    n/=2;
-                    cout<<n<<endl;
-                    if(n==1){
-                        return true;
-                    }
-                }  
-            }
+        if(n==0) return 0;
+        while(n%2==0){
+            n=n/2;
         }
-        return false;
+        return n==1;
     }
 };
