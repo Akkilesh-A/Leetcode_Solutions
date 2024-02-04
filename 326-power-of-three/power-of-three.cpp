72b493207fcda55996eae9d900d5f95d2f01ff12
Spacecentre <136828513+Akkilesh-A@users.noreply.github.com>
2024-02-04 23:07:44 +0530

    Time: 15 ms (56.14%) | Memory: 8.1 MB (21.05%) - LeetSync

---
+++ 326-power-of-three/power-of-three.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==1) return true;
+        else if(n==0) return false;
+        else{
+            while(n%3==0){
+                n=n/3;
+            }
+        }
+        return(n==1);
+    }
+};
