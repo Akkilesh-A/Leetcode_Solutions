82ed4e775aae660dcd2b9fa2b26a4f25db2ba66e
Spacecentre <136828513+Akkilesh-A@users.noreply.github.com>
2024-02-04 22:52:22 +0530

    Time: 1 ms (83.31%) | Memory: 44.8 MB (25.73%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -4,9 +4,7 @@ class Solution {
         int val=nums[0];
         for(int i=0;i<nums.length;i++){
             if(nums[i]>val){
-                int temp=nums[ind];
                 nums[ind]=nums[i];
-                nums[i]=temp;
                 val=nums[ind];
                 ind=ind+1;
             }
