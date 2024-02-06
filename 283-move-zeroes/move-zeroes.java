44a5820d32a7635cda8ece040adc09dcd8f98e61
Spacecentre <136828513+Akkilesh-A@users.noreply.github.com>
2024-02-06 21:52:05 +0530

    Time: 1 ms (100.00%) | Memory: 45.9 MB (59.29%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,9 +1,12 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        if (nums.length == 1) {
+            return;
+        }
         int i=0;
-        for(int j=0;j<nums.length;j++){
-            if(nums[j]!=0){
-                nums[i]=nums[j];
+        for(int n:nums){
+            if(n !=0){
+                nums[i]=n;
                 i++;
             }
         }
