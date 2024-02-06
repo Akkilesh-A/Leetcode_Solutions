afa5355362bbe5a18d0a9fbae4e7eef68b65bf82
Spacecentre <136828513+Akkilesh-A@users.noreply.github.com>
2024-02-06 21:49:46 +0530

    Time: 2 ms (82.53%) | Memory: 46.3 MB (25.58%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,28 +1,40 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int nonzero=0;
-        int zero=0;
-        while(nonzero<nums.length && zero<=nums.length){
-            if(nums[nonzero]==0 && nums[zero]!=0){
-                int temp=nums[nonzero];
-                nums[nonzero]=nums[zero];
-                nums[zero]=temp;
-                nonzero+=1;
+        int i=0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=0){
+                nums[i]=nums[j];
+                i++;
             }
-            else if(nums[nonzero]!=0 && nums[zero]!=0){
-                zero+=1;
-                nonzero+=1;
-            }
-            else if(nums[nonzero]==0 && nums[zero]==0){
-                nonzero+=1;
-            }
-            else{
-                int temp=nums[nonzero];
-                nums[nonzero]=nums[zero];
-                nums[zero]=temp;
-                nonzero+=1;
-                zero+=1;
-            }
+        }
+        while(i<nums.length){
+            nums[i]=0;
+            i++;
         }
     }
 }
+
+// int nonzero=0;
+// int zero=0;
+// while(nonzero<nums.length && zero<=nums.length){
+//     if(nums[nonzero]==0 && nums[zero]!=0){
+//         int temp=nums[nonzero];
+//         nums[nonzero]=nums[zero];
+//         nums[zero]=temp;
+//         nonzero+=1;
+//     }
+//     else if(nums[nonzero]!=0 && nums[zero]!=0){
+//         zero+=1;
+//         nonzero+=1;
+//     }
+//     else if(nums[nonzero]==0 && nums[zero]==0){
+//         nonzero+=1;
+//     }
+//     else{
+//         int temp=nums[nonzero];
+//         nums[nonzero]=nums[zero];
+//         nums[zero]=temp;
+//         nonzero+=1;
+//         zero+=1;
+//     }
+// }
