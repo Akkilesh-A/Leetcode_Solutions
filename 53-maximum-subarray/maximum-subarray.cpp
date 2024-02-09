4223aeea651b0a6bee1ee6c27697bc1588963a59
Spacecentre <136828513+Akkilesh-A@users.noreply.github.com>
2024-02-09 16:07:14 +0530

    Time: 90 ms (30.48%) | Memory: 70.1 MB (35.83%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,30 +1,44 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int max=INT_MIN;
+                int max=INT_MIN;
         int sum=0;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            if(sum<0){
-                sum=0;
-            }
             if(sum>max){
                 max=sum;
             }
-        }
-        int nmax=INT_MIN;
-        int count=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]<0){
-                count+=1;
-                if(nums[i]>nmax){
-                    nmax=nums[i];
-                }
+            if(sum<0){
+                sum=0;
             }
-        }
-        if(count==nums.size()){
-            return nmax;
         }
         return max;
     }
 };
+
+
+        // int max=INT_MIN;
+        // int sum=0;
+        // for(int i=0;i<nums.size();i++){
+        //     sum+=nums[i];
+        //     if(sum<0){
+        //         sum=0;
+        //     }
+        //     if(sum>max){
+        //         max=sum;
+        //     }
+        // }
+        // int nmax=INT_MIN;
+        // int count=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]<0){
+        //         count+=1;
+        //         if(nums[i]>nmax){
+        //             nmax=nums[i];
+        //         }
+        //     }
+        // }
+        // if(count==nums.size()){
+        //     return nmax;
+        // }
+        // return max;
