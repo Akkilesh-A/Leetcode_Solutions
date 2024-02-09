aba9c386b87425fb27737dc7621500486de72eac
Spacecentre <136828513+Akkilesh-A@users.noreply.github.com>
2024-02-09 16:07:38 +0530

    Time: 92 ms (24.91%) | Memory: 70.2 MB (14.11%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-                int max=INT_MIN;
+        int max=INT_MIN;
         int sum=0;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
