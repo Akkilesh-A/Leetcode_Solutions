10ba65589960b932ce85099d62c71a80fca4284d
Spacecentre <136828513+Akkilesh-A@users.noreply.github.com>
2024-02-26 18:52:05 +0530

    Time: 6 ms (51.31%) | Memory: 39.6 MB (99.58%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,6 +1,8 @@
 class Solution {
     public int fib(int n) {
         if(n<=1) return n;
+        else if(n==2) return 1;
+        else if(n==3) return 2;
         return fib(n-1)+fib(n-2);
     }
 }
